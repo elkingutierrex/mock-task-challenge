1f320c6bdb84f8829986f50892165eb7dab83e79
elkingutierrex <elkin.gutierrex@gmail.com>
2026-02-10 08:59:03 -0500

    feat(taskModel): create enum and interfaces

--- src/app/shared/models/task.model.ts
+++ src/app/shared/models/task.model.ts
@@ -1,16 +1,18 @@
+export type Priority = 'low' | 'medium' | 'high';
+
 export interface UserClaims {
-    sub: string;
-    email: string;
-    name: string;
-    roles: string[];
-    exp: number;
+  sub: string;
+  email: string;
+  name: string;
+  roles: string[];
+  exp: number;
 }
 
 export interface Task {
-    id: number;
-    title: string;
-    completed: boolean;
-    userId: number;
-    priority?: 'low' | 'medium' | 'high';
-    dueDate?: Date;
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+  priority?: Priority;
+  dueDate?: Date;
 }
